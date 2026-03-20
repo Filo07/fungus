259c8547150d887a2e81b49ed8c524e38b275d0d
Filo <filipingback@gmail.com>
2026-03-20 12:32:35 +0100

    v0.7

--- index.php
+++ index.php
@@ -15,6 +15,15 @@
             <h1>Endure and Survive</h1>
             <img src="bilder/frontpage.jpg" alt="Ellie hiding behind a tree">
         </div>
+        <section class="fronttext">
+            <p>Welcome to our website dedicated to The Last of Us, a critically acclaimed action-adventure game 
+                developed by Naughty Dog.<br><br>Here, you can explore the rich story, meet the unforgettable characters, 
+                and discover the unique world of The Last of Us. Whether you're a longtime fan or new to the series, 
+                we invite you to dive into the post-apocalyptic universe where survival is a daily struggle and every decision matters.
+                <br><br>
+                Join us as we delve into the heart of this unforgettable journey.
+            </p>
+        </section>
         <div class="frontknappar">
             <a href="story.php" class="buttON">The Story</a>
             <a href="char.php" class="buttON">The Characters</a>
--- story.php
+++ story.php
@@ -19,21 +19,24 @@
             <article>
                 <p><bold>The Last of Us</bold> takes place in a world devastated by a deadly fungal infection that has collapsed civilization. Cities lie abandoned, governments have fallen, and survivors live in heavily controlled quarantine zones or dangerous wastelands.
                 The outbreak was caused by a mutated strain of the Cordyceps fungus, which infects humans and slowly turns them into aggressive, mindless hosts. As the infection spread across the globe, society quickly descended into chaos.
-                Twenty years after the initial outbreak, the story follows <bold>Joel Miller</bold>, a hardened survivor who has lost almost everything. His life changes when he is tasked with escorting a teenage girl, <bold>Ellie Williams</bold>, across the United States.<br>
-                <bold>Ellie</bold> may be humanity’s last hope<br>
-                Unlike others, she is immune to the infection. A resistance group known as the Fireflies believes her immunity could help create a cure. What begins as a simple job slowly turns into a brutal journey through ruined cities, hostile survivors, and infected creatures.<br>
-                Along the way, <bold>Joel</bold> and <bold>Ellie</bold> form a powerful bond, learning to trust each other in a world where trust can be deadly.<br>
-                At its core, The Last of Us is not just about survival—it’s about loss, loyalty, and how far someone will go to protect the people they love.</p>
+                Twenty years after the initial outbreak, the story follows <bold>Joel Miller</bold>, a hardened survivor who has lost almost everything. His life changes when he is tasked with escorting a teenage girl, <bold>Ellie Williams</bold>, across the United States.<br><br>
+                <bold>Ellie</bold> may be humanity’s last hope<br><br>
+                Unlike others, she is immune to the infection. A resistance group known as the Fireflies believes her immunity could help create a cure. What begins as a simple job slowly turns into a brutal journey through ruined cities, hostile survivors, and infected creatures.<br><br>
+                Along the way, <bold>Joel</bold> and <bold>Ellie</bold> form a powerful bond, learning to trust each other in a world where trust can be deadly.<br><br>
+                At its core, The Last of Us is not just about survival—it’s about loss, loyalty, and how far someone will go to protect the people they love.</p><br><br>
             </article>
             <div class="mushroom">
                 <article>
-                    <h1>Cordyceps</h1>
-                    <p>The cordyceps fungus in The Last of Us is a fictional infection that causes a global collapse of society. 
+                    <details>
+                        <summary><h1>Cordyceps</h1></summary>
+                        <p>The cordyceps fungus in The Last of Us is a fictional infection that causes a global collapse of society. 
                         It is inspired by the real fungus Ophiocordyceps unilateralis, which can control the behavior of insects. 
-                        In the story, the fungus evolves to infect humans, spreading through bites, infected food, or contact with fungal growth. 
+                        In the story, the fungus evolves to infect humans, spreading through bites, infected food, or contact with fungal growth.<br><br> 
                         Once inside the body, it attacks the brain, takes control of the host, and slowly transforms the person into a violent, 
                         fungus-covered creature that helps spread the infection further.
                         Over time, infected people change into different stages as the fungus grows.</p>
+
+                    </details>
                 </article>
             </div>
         </section>
